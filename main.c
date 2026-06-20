c5644862c9c86808cbc6617fc068f5428a84a0f0
Fernando Simplicio de Sousa <fernando@microgenios.com.br>
2026-06-20 15:12:56 -0300

    Print senai

--- main.c
+++ main.c
@@ -24,6 +24,7 @@
  */
 int main( void )
 {
+	printf("senai");
 	/**
 	 * Para usar qualquer GPIO antes é necessário configurar o sentido (entrada ou saída);
 	 * Configura todos os pinos do PORTB como saída; 
